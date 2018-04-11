c3d8431e3863854c1f2eee9c5d2d242f6ec4c5df
eddardd <edumachine@hotmail.com>
2018-04-10 22:07:38 -0300

    Updated codes

--- Advanced-Control/Codes/Python/roots.py
+++ Advanced-Control/Codes/Python/roots.py
@@ -19,3 +19,20 @@ sys_unstable = linear_equations(sp = 0)
 
 poles_unstable, _ = np.linalg.eig(A_unstable)
 poles_stable, _ = np.linalg.eig(A_stable)
+
+fig, ax = plt.subplots(1,2,figsize=(14,6),sharey=True)
+plt.suptitle('Eigenvalues of linearized system')
+
+ax[0].set_title(r'$\theta_{sp} = 0$')
+ax[0].plot(poles_unstable, [0,0,0,0], 'rx')
+ax[0].set_xlabel('Real')
+ax[0].set_ylabel('Imaginary')
+ax[0].grid()
+ax[0].axvline()
+
+ax[1].set_title(r'$\theta_{sp} = \pi$')
+ax[1].plot(np.real(poles_stable), np.imag(poles_stable), 'rx')
+ax[1].set_xlabel('Real')
+ax[1].set_ylabel('Imaginary')
+ax[1].grid()
+ax[1].axvline()
